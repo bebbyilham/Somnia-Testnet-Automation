7365f064660f6ccd6edba1f457f135e79891d26f
sinox0101 <129672206+sinak1023@users.noreply.github.com>
2025-05-03 01:30:05 +0330

    Update mintsusdt.py

--- scripts/mintsusdt.py
+++ scripts/mintsusdt.py
@@ -183,7 +183,6 @@ def run_mintsusdt():
                 successful_mints += 1
 
     print_border(f"COMPLETED: {successful_mints}/{total_wallets} TRANSACTIONS SUCCESSFUL", Fore.GREEN)
-    print(f"{Fore.CYAN}Developed by Ostad kachal | GitHub: https://github.com/sinak1023}{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     run_mintsusdt()
